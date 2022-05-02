b91a5f4044e18216238ea1894a1f8a10913cd0cd
jachin <2061979589@qq.com>
2022-05-02 10:01:59 +0800

    初始化并梳理项目的结构

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
 
-createApp(App).use(store).use(router).mount('#app')
+import 'amfe-flexible'
+
+// 加载全局样式
+import 'vant/lib/index.css'
+import './styles/index.less'
+
+createApp(App).use(store).use(router).use(Vant).mount('#app')
